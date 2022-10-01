3e177c9ec549a6102c1b98448b47be41a601efe9
CollinsNasongo <nasongocollins@gmail.com>
2022-10-01 22:48:23 +0300

    Module for the file organizer project

---
+++ folder_organizer/extensions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+extensions = {
+        "3g2": "Video", "3gp": "Video", "avi": "Video", "flv": "Video", "h264": "m4v", "mkv": "Video", "mov": "Video", "mp4": "Video", "mpg": "Video", "mpeg": "Video", "rm": "Video", "swf": "Video", "vob": "Video", "wmv": "Video", "ts": "Video",
+        "aif": "Audio", "cda": "Audio", "mid": "Audio", "mp3": "Audio", "mpa": "Audio", "ogg": "Audio", "wav": "Audio", "wma": "Audio", "wma": "Audio",
+        "doc": "Text", "odt": "Text","rtf": "Text", "tex": "Text", "txt": "Text", "wks": "Text", "wps": "Text","wpd": "Text", "pdf": "Text",
+        "jpg": "Pictures", "png": "Pictures", "jfif": "Pictures",
+        "pptx": "Presentations", "ppt": "Presentations", "pps": "Presentations","odp": "Presentations","key": "Presentations",
+        "ods": "Spreadsheets","xlr": "Spreadsheets", "xls": "Spreadsheets", "xlsx": "Spreadsheets",
+        "7z": "Compressed", "arj": "Compressed","deb": "Compressed", "pkg": "Compressed", "rar": "Compressed", "zip": "Compressed", "rpm": "Compressed", "tar": "Compressed", "z": "Compressed",
+        "csv": "Databases", "dat": "Databases","db": "Databases", "log": "Databases", "mdb": "Databases", "sav": "Databases", "sql": "Databases", "tar": "Databases", "xml": "Databases",
+        "apk": "Executables", "bat": "Executables", "bin": "Executables", "cgi": "Executables", "com": "Executables", "exe": "Executables", "gadget": "Executables", "jar": "Executables", "py": "Executables", "wsf": "Executables",
+        "asp": "Web", "cer": "Web", "cfm": "Web", "cgi": "Web", "css": "Web", "html": "Web", "js": "Web", "part": "Web", "php": "Web", "rss": "Web", "xhtml": "Web",
+        "c": "Programming","class": "Programming", "cpp": "Programming", "cs": "Programming", "h": "Programming", "java": "Programming", "sh": "Programming", "swift": "Programming", "vb": "Programming"
+        }
