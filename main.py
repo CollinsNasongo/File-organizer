2675b7063d41fce1033e884cd69e1d3a6e477359
CollinsNasongo <nasongocollins@gmail.com>
2022-10-02 01:44:44 +0300

    The main file

--- main.py
+++ main.py
@@ -3,8 +3,31 @@
 
 from file_organizer.extensions import extensions
 from file_organizer.directory_structure import create
-def main():
+from file_organizer.directories import get_to_test_folder, get_dirs, get_files, get_extension, make_folder
+import shutil
 
+def main():
+   # get the files
+   files = get_files()
+   folders = get_dirs()
+
+   # add every file to the appropriate folder
+   for file in files:
+       ext = get_extension(file)
+
+       if (ext in extensions):
+           folder_name = extensions[ext]
+
+       else:
+           folder_name = "Other"
+           pass
+       if folder_name in folders:
+            shutil.move(file, folder_name)
+            pass
+       else:
+            make_folder(folder_name)
+            shutil.move(file, folder_name)
 
 if __name__ == "__main__":
+    get_to_test_folder()
     main()
